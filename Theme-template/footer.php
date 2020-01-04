6088d0feab458c24c20df50ca55cd91423b9f870
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-01-04 17:32:07 +0800

    Add files via upload

---
+++ Theme-template/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nusscistudentlife
+ */
+?>
+
+	</div><!-- #content -->
+
+	<footer class="container-fluid py-3 bg-light">
+		<div class="row">
+			<div class="col-md-4" style="height:150px;">
+				<p>Footer content contained here</p>
+			</div>
+			<div class="col-md-8">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis porta purus, in vestibulum magna ullamcorper in. Sed ac bibendum urna, et vehicula dui. Aliquam luctus enim euismod nisl interdum, id suscipit quam sollicitudin. Proin vitae massa vitae magna scelerisque pulvinar. Curabitur posuere eu ex eget feugiat. Suspendisse bibendum pharetra metus ut commodo. Donec in diam nec magna semper pretium. Integer lectus nibh, sagittis mollis finibus eu, imperdiet quis quam.</p>
+			</div>
+		</div>
+	</footer><!-- #colophon -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
