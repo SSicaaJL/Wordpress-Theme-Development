16095b4bb02d8907af947dbce958767ec98a6787
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-01-04 18:13:56 +0800

    Add files via upload

---
+++ Theme-template/front-page.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nusscistudentlife
+ */
+
+get_header();
+?>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col" style="height: 70vh;">
+			<p>This is the front page, containers and site dynamics to be implemented</p>
+		</div>
+	</div>
+</div>
+
+<?php
+get_footer();
+?>
