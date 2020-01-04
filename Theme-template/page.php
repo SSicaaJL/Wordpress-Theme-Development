ae4a0039ec4c353e16b77d0397b069efd1bb5dc2
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-01-05 02:49:48 +0800

    Update page.php

--- Theme-template/page.php
+++ Theme-template/page.php
@@ -12,8 +12,13 @@ get_header();
 
 <main class="container-fluid">
 	<div class="row">
-		<div class="col">
+		<div class="col-md-2">
 			<h1>page.php has loaded</h1>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div class="light-bg"><h1><?php the_title();?></h1></div>
+				<?php the_content();
+				endwhile;
+			endif;?>
 		</div>
 	</div>
 </main>
