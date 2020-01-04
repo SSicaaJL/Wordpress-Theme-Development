4020795add7d9ff8ccdc7c65244c7a9af0b08590
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-01-05 01:08:36 +0800

    Add files via upload

---
+++ Theme-template/page.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * nusscistudentlife page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nusscistudentlife
+ */
+
+get_header();
+?>
+
+<main class="container-fluid">
+	<div class="row">
+		<div class="col">
+			<h1>page.php has loaded</h1>
+		</div>
+	</div>
+</main>
+
+<?php
+get_footer();
+?>
