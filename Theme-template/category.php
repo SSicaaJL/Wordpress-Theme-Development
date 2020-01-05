ee2857ce63a5c1aeadf607f0298cf040d829231d
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-01-05 22:35:34 +0800

    Add files via upload

---
+++ Theme-template/category.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * nusscistudentlife category page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nusscistudentlife
+ */
+
+get_header();
+?>
+
+<div class="container-fluid customcontent">
+	<p>debug: category.php has loaded</p>
+</div>
+
+<main class="container-fluid">
+	<div class="row">
+		<div class="col">
+			<?php if ( have_posts() ) : 
+				while ( have_posts() ) : the_post();
+					the_title();
+					the_content();
+				endwhile; 
+			endif;?>
+		</div>
+	</div>
+
+</main>
+
+<?php
+get_footer();
+?>
