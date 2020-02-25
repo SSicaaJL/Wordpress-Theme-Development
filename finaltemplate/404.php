b52bd1c84e7bb600451494e5e0059963e0d2e804
SSicaaJL <43140909+SSicaaJL@users.noreply.github.com>
2020-02-25 22:50:52 +0800

    Add files via upload

---
+++ finaltemplate/404.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * nusscistudentlife category page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package nusscistudentlife
+ */
+
+get_header();
+?>
+
+<div class="container-fluid">
+	<h2>Page not found</h2>
+</div>
+
+
+<div class="container-fluid row align-items-end">
+<?php
+get_footer();
+?>
+</div>
