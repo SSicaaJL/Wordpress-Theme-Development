00b7356bd3006784e3574c97d9924cc2da2bc415
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-02-25 20:30:31 +0800

    Update page.php

--- finaltemplate/page.php
+++ finaltemplate/page.php
@@ -10,39 +10,41 @@
 get_header();
 ?>
 
-<main class="container-fluid">
+<main class="container-xl">
 	<div class="row">
-		<div class="col-md-9 py-3">
+		<div class="col-md justify-content-center pt-3">
 			<?php if ( have_posts() ) : 
 				while ( have_posts() ) : the_post(); ?>
-					<?php 
-					the_content();
+					<h3><?php the_title(); ?></h3>
+					<p><?php the_content(); ?></p><?php
 				endwhile; 
 			endif;?>
 		</div>
+		
+		<!-- SIDE BAR -->
+		<?php
+			$args = array(
+				'post_type'      => 'page',
+				'posts_per_page' => -1,
+				'post_parent'    => $post->ID,
+				'order'          => 'ASC',
+				'orderby'        => 'menu_order'
+			);
 
-		<div class="col-3"> <!-- SIDE BAR -->
-			<div class="sci-bg-red-soft my-3 p-3" style="min-height: 600px;">
-			<?php
-				$args = array(
-					'post_type'      => 'page',
-					'posts_per_page' => -1,
-					'post_parent'    => $post->ID,
-					'order'          => 'ASC',
-					'orderby'        => 'menu_order'
-				);
+			$parent = new WP_Query( $args );
 
-				$parent = new WP_Query( $args );
-
-				if ( $parent->have_posts() ) :
-					while ( $parent->have_posts() ) : $parent->the_post(); ?>
-            				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><br>
-					<?php 
-					endwhile;
-				endif; 
-				wp_reset_postdata(); ?>
-			</div>
-		</div>
+			if ( $parent->have_posts() ) :?>
+			<div class="col-md-3 pt-3">
+				<div><h3>Getting Around</h3>
+					<?php
+				while ( $parent->have_posts() ) : $parent->the_post(); ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a><br>
+				<?php 
+				endwhile;?>
+				</div>
+			</div><?php
+			endif; 
+			wp_reset_postdata(); ?>
 	</div>
 
 </main>
