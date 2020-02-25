524e3a0d6abf49c2b890c54978606591280c86f9
SSicaaJL <43140909+SSicaaJL@users.noreply.github.com>
2020-02-25 22:52:27 +0800

    Update 404.php

--- finaltemplate/404.php
+++ finaltemplate/404.php
@@ -1,3 +1,4 @@
+
 <?php
 /**
  * nusscistudentlife category page
@@ -9,14 +10,21 @@
 
 get_header();
 ?>
-
-<div class="container-fluid">
-	<h2>Page not found</h2>
-</div>
-
-
-<div class="container-fluid row align-items-end">
-<?php
-get_footer();
-?>
+<div class="container-fluid" style="height:90vh;" >
+	<div class="row">
+		<div class="col">
+			<center>
+			<img src="http://localhost:8888/nus/wp-content/uploads/2020/02/dog-cute-404.jpg" alt="Responsive image" style="height:70vh; width:60%" class="border border-dark">
+			</center>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col">
+			<center>
+			<h1 style="font-family:ageo;">
+			<p>Whoops, Tucker may have chewed the power cords!</p>
+			</h1>
+			</center>
+		</div>
+	</div>
 </div>
