f8e1d5b4e6d3437b78ce0260d95bade09ce88a36
tincaiwachi <59481883+tincaiwachi@users.noreply.github.com>
2020-02-28 05:17:44 +0800

    Update footer.php

--- finaltemplate/footer.php
+++ finaltemplate/footer.php
@@ -1,61 +1,32 @@
 <?php
 /**
- * The template for displaying the footer
+ * nusscistudentlife login page
  *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package nusscistudentlife
  */
+
+get_header();
 ?>
 
-	</div><!-- #content -->
+<main class="container-xl">
+	<div class="row pt-3">
+		<?php if ( is_user_logged_in() ) { ?>
+			<div class="col">
+				<p>Welcome back!</p>
+			</div><?php
+		} else { ?>
+			<div class="col-md">
+				<?php echo do_shortcode("[wpmem_form login]"); ?>
+			</div>
+			<div class="col-md">
+				<?php echo do_shortcode("[wpmem_form register]"); ?>
+			</div><?php
+		} ?>
+	</div>
+</main>
 
-	<!-- FOOTER CONTAINER -->
-	<footer class="container-fluid pt-3 sci-bg-red-soft">
-
-		<!-- FIRST(TOP) PART OF THE FOOTER -->
-		<div class="row pt-3 text-center text-light">
-
-			<!-- SETTING UP WIDGETS AREAS IN COLUMNS -->
-			<?php //Left widget
-				if ( is_active_sidebar( 'footer-widget-l' ) ) {?>
-					<div class="col-md"><?php
-					dynamic_sidebar( 'footer-widget-l' );?>
-					</div><?php
-				}
-			?>
-			<?php //Center widget
-				if ( is_active_sidebar( 'footer-widget-c' ) ) {?>
-					<div class="col-md"><?php
-					dynamic_sidebar( 'footer-widget-c' );?>
-					</div><?php
-				}
-			?>
-			<?php //Right widget
-				if ( is_active_sidebar( 'footer-widget-r' ) ) {?>
-					<div class="col-md"><?php
-					dynamic_sidebar( 'footer-widget-r' );?>
-					</div><?php
-				}
-			?>
-		</div><hr class="hr-white">
-
-		<!-- SECOND(BOTTOM) PART OF THE FOOTER -->
-		<div class="row pb-3 text-center text-light" style="font-size: 0.8rem;">
-			<?php //Bottom widget
-				if ( is_active_sidebar( 'footer-widget-b') ) {?>
-					<div class="col justify-content-center"><?php
-					dynamic_sidebar( 'footer-widget-b' );?>
-					</div><?php
-				}
-			?>
-		</div>
-	</footer>
-	<!-- /FOOTER CONTAINER -->
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
+<?php
+get_footer();
+?>
